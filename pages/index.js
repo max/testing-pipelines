30c3e23f45f4a43c2b3006f0a6da91ee6b8bed5e
Max Schoening <max@max.wtf>
2017-01-22 15:47:44 -0800

    Style comps
    - Include Tachyons and normalize.css
    - Center comps horizontally
    - Add a buttom margin to comps unless it's the last one

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from '../components/image';
 
 const comps = [
@@ -13,17 +14,28 @@ const comps = [
   'screenshots'
 ];
 
+const isLast = (index, items) => ( index + 1 === items.length );
+
 export default () => (
-  <div>
-    <h1>My Work</h1>
+  <div className="wrapper center pv4">
+    <Head>
+      <link rel="stylesheet" href="https://unpkg.com/tachyons@4.6.1/css/tachyons.min.css" />
+      <link rel="stylesheet" href="https://unpkg.com/normalize.css@5.0.0/normalize.css" />
+    </Head>
 
     {
       comps.map((comp, index) => (
-        <Image key={ index } name={ comp } />
+        <div className={ !isLast(index, comps) && 'mb4' } key={ index }>
+          <Image name={ comp } />
+        </div>
       ))
     }
 
     <style jsx>{`
+      .wrapper {
+        max-width: 1200px;
+      }
+
       img {
         display: block;
       }
