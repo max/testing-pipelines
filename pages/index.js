0712aa1b3cc05c6d67fb27cc136e09c6c8ee0f18
Max Schoening <max@max.wtf>
2017-01-22 14:58:34 -0800

    Populate index page with mockups

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
+const comps = [
+  'cli',
+  'dashboard-v1',
+  'dashboard-v2',
+  'slider',
+  'status',
+  'button',
+  'dropbox',
+  'concept',
+  'preview',
+  'screenshots'
+];
+
 export default () => (
-  <h1>My personal internet website</h1>
+  <div>
+    <h1>My Work</h1>
+
+    {
+      comps.map((comp, index) => (
+        <img key={ index } src={ `/static/images/${comp}.jpg` } />
+      ))
+    }
+
+    <style jsx>{`
+      img {
+        display: block;
+      }
+    `}</style>
+  </div>
 );
