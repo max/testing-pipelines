93b7270dac7bbabf57fa40321c2e108181facff7
Max Schoening <max@max.wtf>
2017-01-25 10:04:04 -0800

    Improve image loading experience and accessibility
    - Add image height and width attributes to prevent thrashing while
      loading
    - Add alt attributes for acessibility

--- components/image.js
+++ components/image.js
@@ -2,6 +2,12 @@ const path = (name, suffix='') => {
   return `/static/images/${name}${suffix}.jpg`;
 };
 
-export default ({ name, alt }) => (
-  <img alt={ alt } className='db' src={ path(name) } srcSet={ `${path(name)}, ${path(name, '@2x')} 2x` } />
+export default ({ alt, height, src, width }) => (
+  <img
+    alt={ alt }
+    className='db'
+    height={ height }
+    src={ path(src) }
+    srcSet={ `${path(src)}, ${path(src, '@2x')} 2x` }
+    width={ width } />
 );
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import Head from 'next/head';
 import Image from '../components/image';
 
 const comps = [
-  'cli',
-  'dashboard-v1',
-  'dashboard-v2',
-  'slider',
-  'status',
-  'button',
-  'dropbox',
-  'concept',
-  'preview',
-  'screenshots'
+  { src: 'cli', title: 'Heroku CLI', height: 800, width: 1200 },
+  { src: 'dashboard-v1', title: 'Heroku Dashboard V1', height: 972, width: 1200 },
+  { src: 'dashboard-v2', title: 'Heroku Dashboard V2', height: 944, width: 1200 },
+  { src: 'slider', title: 'Heroku Process Slider', height: 680, width: 1200 },
+  { src: 'status', title: 'Heroku Status', height: 1507, width: 1200 },
+  { src: 'button', title: 'Heroku Button', height: 1406, width: 1200 },
+  { src: 'dropbox', title: 'Heroku Dropbox Sync', height: 594, width: 1200 },
+  { src: 'concept', title: 'CloudApp Icon &amp; Concept', height: 795, width: 1200 },
+  { src: 'preview', title: 'CloudApp Preview', height: 750, width: 1200 },
+  { src: 'screenshots', title: 'CloudApp Screenshots', height: 150, width: 1200 }
 ];
 
 const isLast = (index, items) => ( index + 1 === items.length );
@@ -30,7 +30,11 @@ export default () => (
     {
       comps.map((comp, index) => (
         <div className={ `comp ${!isLast(index, comps) && 'mb4'}` } key={ index }>
-          <Image name={ comp } />
+          <Image
+            alt={ comp.title }
+            height={ comp.height }
+            src={ comp.src }
+            width={ comp.width } />
         </div>
       ))
     }
@@ -42,10 +46,6 @@ export default () => (
 
       .comp {
         box-shadow: 0 1px 10px rgba(0, 0, 0, 0.25);
-      }
-
-      img {
-        display: block;
       }
     `}</style>
 
